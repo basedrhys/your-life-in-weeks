9e43f4906af4161ea6e852893173c1c552b7284d
Rhys <rhys.compton@gmail.com>
2020-06-28 13:04:29 +1200

    Canvas correctly sizes itself based on box size

--- popup.js
+++ popup.js
@@ -7,9 +7,14 @@ const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 const numWeeks = 52;
 const numYears = 90;
-const boxScale = 0.35;
-const xMult = 30, yMult = 30, boxWidth = 20, boxHeight = 20;
+const boxSize = 12, boxGap = 6;
+const boxMult = boxSize + boxGap
 
+// Set the canvas size
+canvas.width = boxMult * numWeeks - boxGap;
+canvas.height = boxMult * numYears - boxGap;
+ctx.fillStyle = "#FFF";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 let pastColor = "#FF5252";
 let currentColor = "#4CAF50"
@@ -45,15 +50,22 @@ chrome.storage.sync.get(['birthDateSet'], function(result) {
     let weekCount = 0
     
     for (let year = 0; year < numYears; year++) {
-      for (let week = 0; week < numWeeks; week++) {
-        ctx.fillRect((week * xMult) * boxScale, (year * yMult) * boxScale, boxWidth * boxScale, boxHeight * boxScale);
-        
+      for (let week = 0; week < numWeeks; week++) {        
         weekCount++;
         if (weekCount === doneWeeks) {
           ctx.fillStyle = currentColor;
         } else if (weekCount > doneWeeks) {
-          ctx.fillStyle = futureColor;
+          // if (year == numYears - 1) 
+          //   ctx.fillStyle = "#FF0"
+          // else if (week == numWeeks - 1)
+          //   ctx.fillStyle = "#F0F"
+          // else
+            ctx.fillStyle = futureColor;
         }
+
+        let xPos = week * boxMult
+        let yPos = year * boxMult
+        ctx.fillRect(xPos, yPos, boxSize, boxSize);
       }
     }
 
