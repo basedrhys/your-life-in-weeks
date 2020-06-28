02ecb98444cc8c0d1db92ffda9811b5289a5a157
Rhys <rhys.compton@gmail.com>
2020-06-28 14:22:43 +1200

    Move files into separate folders

---
+++ js/background.js
@@ -0,0 +1,10 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({birthDateSet: true});
+  chrome.storage.sync.set({birthYear: 1990, birthMonth: 11, birthDay: 20});
+});
