cfdd97aa20de1140bf4f1810398bf057ccd02df8
rhyscompton <rhys.compton@gmail.com>
2020-07-06 16:25:52 +1200

    Refactored drawMyLife out to drawing.js

--- js/drawing.js
+++ js/drawing.js
@@ -1,7 +1,11 @@
+import { saveCanvasToImage } from "./util.js";
+
 export function setupCanvas() {
     // Set the canvas size
     canvas.width = boxAndMarginSize * numWeeks - boxMargin;
     canvas.height = boxAndMarginSize * numYears - boxMargin;
+
+    clearCanvas();
 }
 
 export function clearCanvas() {
@@ -9,19 +13,77 @@ export function clearCanvas() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-export function drawThisWeek(currWeekX, currWeekY) {
+export function drawDay(week, year) {
+    let xPos = week * boxAndMarginSize
+    let yPos = year * boxAndMarginSize
+
+    ctx.fillRect(xPos, yPos, boxSize, boxSize);
+}
+
+export function drawMyLife(doneWeeks) {
+    let weekCount = 0
+    
+    let currWeek, currYear;
+    
+    for (let year = 0; year < numYears; year++) {
+      for (let week = 0; week < numWeeks; week++) {        
+        // Set the current fill style
+        ctx.fillStyle = pastColor;
+                
+        if (weekCount > doneWeeks) {
+          ctx.fillStyle = futureColor;
+          
+          if (testMode) {
+            drawBorder(week, year);
+          } 
+        }
+        
+        drawDay(week, year)
+
+        // Save the position for drawing this week later
+        if (weekCount === doneWeeks) {
+          currWeek = week;
+          currYear = year;
+        } 
+
+        weekCount++;
+      }
+    }
+    
+    drawThisWeek(currWeek, currYear)
+    
+    saveCanvasToImage();
+}
+
+export function drawThisWeek(currWeek, currYear) {
     let currWeekBoxSize = boxSize + (currWeekBoxIncrease * 2)
 
+    // Convert week and year to actual x and y positions
+    let xCorrected = (currWeek * boxAndMarginSize)
+    let yCorrected = (currYear * boxAndMarginSize)
+
+    // This week is drawn bigger so set the x and y to match that
+    xCorrected = xCorrected - currWeekBoxIncrease;
+    yCorrected = yCorrected - currWeekBoxIncrease;
+
     // Draw the outline - a black filled box slightly bigger
-    ctx.fillRect(currWeekX-lineWidth, 
-                currWeekY-lineWidth, 
     ctx.fillStyle = outlineColor
+    ctx.fillRect(xCorrected-lineWidth, 
+                yCorrected-lineWidth, 
                 currWeekBoxSize + (lineWidth * 2), 
                 currWeekBoxSize + (lineWidth * 2));
 
     ctx.fillStyle = currentColor;
-    ctx.fillRect(currWeekX, 
-                currWeekY, 
+    ctx.fillRect(xCorrected, 
+                yCorrected, 
                 currWeekBoxSize, 
                 currWeekBoxSize);
+}
+
+export function drawBorder(week, year) {
+    if (year == 0 || year == numYears - 1 ||
+        week == 0 || week == numWeeks - 1) 
+        ctx.fillStyle = testOutsideColor
+    else
+        ctx.fillStyle = futureColor;
 }
--- js/popup.js
+++ js/popup.js
@@ -1,66 +1,25 @@
 'use strict';
 
-import { calcDoneWeeks, setMessage, setupButtonListeners, saveCanvasToImage } from "./util.js";
-import { clearCanvas, drawThisWeek, setupCanvas } from "./drawing.js"
-
-setupCanvas();
-clearCanvas();
-
+import { calcDoneWeeks, setupButtonListeners } from "./util.js";
+import { setupCanvas, drawMyLife } from "./drawing.js"
 
 setupButtonListeners();
 
-// Set the input values if we've already saved a birthdate
-chrome.storage.sync.get(['birthDateSet'], function(result) {
-  let birthDateSet = result.birthDateSet
-  if (!birthDateSet) {
-    setMessage("No birthdate set, please set this in the extension options")
-    return
-  }
-  // Get the rest of the birth values
-  chrome.storage.sync.get([
-    'birthYear',
-    'birthMonth',
-    'birthDay'
-  ], function(birthValues) {
-    let doneWeeks = calcDoneWeeks(
-      birthValues.birthYear,
-      birthValues.birthMonth,
-      birthValues.birthDay)
-
-    let weekCount = 0
-
-    let currWeekX, currWeekY;
+// Get the rest of the birth values
+chrome.storage.sync.get([
+  'birthYear',
+  'birthMonth',
+  'birthDay'
+], function(birthValues) {
+  
+  
+  let doneWeeks = calcDoneWeeks(
+    birthValues.birthYear,
+    birthValues.birthMonth,
+    birthValues.birthDay)
     
-    for (let year = 0; year < numYears; year++) {
-      for (let week = 0; week < numWeeks; week++) {        
-        weekCount++;
-
-        let xPos = week * boxAndMarginSize
-        let yPos = year * boxAndMarginSize
-
-        ctx.fillStyle = pastColor;
-        
-        if (weekCount === doneWeeks) {
-          currWeekX = xPos - currWeekBoxIncrease;
-          currWeekY = yPos - currWeekBoxIncrease;
-        } else if (weekCount > doneWeeks) {
-          // if (year == numYears - 1) 
-          //   ctx.fillStyle = "#FF0"
-          // else if (week == numWeeks - 1)
-          //   ctx.fillStyle = "#F0F"
-          // else
-            ctx.fillStyle = futureColor;
-        }
-
-
-        ctx.fillRect(xPos, yPos, boxSize, boxSize);
-      }
-    }
-
-    drawThisWeek(currWeekX, currWeekY)
-
-    saveCanvasToImage();
-  })
+    setupCanvas();
+    drawMyLife(doneWeeks)
 });
-
-
+  
+  
